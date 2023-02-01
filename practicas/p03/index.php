73f6ad87f5aa70dfc48b09c8d3d4b6bb91abc26d
KevArmas <kevin.armas@alumno.buap.mx>
2023-02-01 12:05:50 -0600

    Inciso 6 p03

--- practicas/p03/index.php
+++ practicas/p03/index.php
@@ -1,6 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html lang="en">
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN” “http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="EN”">
 <head>
     <title> Practica3 </title>
 </head>
@@ -13,18 +12,124 @@
     echo '$_myvar es correcta <br>';
 
     $_7var = 11;
-    echo '$_myvar es correcta <br>';
+    echo '$_7var es correcta <br>';
 
     //myvar = 'hola';
     echo 'myvar es incorrecta debido a que le falta el signo de $ al inicio de la variable<br>';
 
     $myvar = 'Prueba';
-    echo '$_myvar es correcta <br>';
+    echo '$myvar es correcta <br>';
 
     $var7 = 33;
-    echo '$_myvar es correcta <br>';
+    echo '$var7 es correcta <br>';
 
+    $_element1 = "pruebas";
+    echo '$_element1 es correcta <br>';
+
+    //$house*5 = 85261;
+    echo '$house*5 es incorrecta ya que tiene un caracter no valido "*"<br><br>';
 
     ?>
+
+    <h1>Inciso 2</h1>
+    <p>2. Proporcionar los valores de $a, $b, $c como sigue: </p>
+    <p>$a = “ManejadorSQL”;<br> $b = 'MySQL’;<br> $c = &$a;<br> </p>
+
+    <?php
+    $a = "ManejadorSQL";
+    $b = 'MYSQL';
+    $c = &$a;
+
+    echo 'Contenido de $a: '.$a.'<br>';  
+    echo 'Contenido de $b: '.$b.'<br>';
+    echo 'Contenido de $c: '.$c.'<br>';
+    
+    $a = "PHP server";
+    $b = &$a;
+
+    echo 'Contenido de $a segunda impresion: '.$a.'<br>';  
+    echo 'Contenido de $b segunda impresion: '.$b.'<br>';
+    echo 'Contenido de $c segunda impresion: '.$c.'<br>';
+    ?>
+
+    <p>En el segunda asignacion de variables, la variable $a se le asigna el texto PHP server
+       por lo cual a partir de esa linea solo mostrara el nuevo texto o sea PHP server </p>
+    <p>A la variable "$b" se le hace una asignacion de tipo apuntador a la variable "$a", por lo que
+       todo lo que muestre "$a" mostrara "$b."</p> 
+    <p>Y finalmente "$c" no tiene ningun cambio pero ya que era apuntador de $a desde el inicio
+       mostrara tambien lo que tenga "$a"</p>
+
+    <h1>Inciso 3</h1>
+    <p>3. Muestra el contenido de cada variable inmediatamente después de cada asignación,
+    verificar la evolución del tipo de estas variables (imprime todos los componentes de los
+    arreglo): </p>
+    <p>$a = “PHP5”;<br>
+        $z[] = &$a;<br>
+        $b = “5a version de PHP”;<br>
+        $c = $b*10;<br>
+        $a .= $b;<br>
+        $b *= $c;<br>
+        $z[0] = “MySQL”;<br> </p>
+    
+    <?php
+        $a = "PHP5";
+        echo 'Contenido de $a: '.$a.'<br>';
+        $z[] = &$a;
+        echo 'Contenido de $z[]: '; print_r($z);
+        $b = "5a version de PHP";
+        echo '<br>'.'Contenido de $b: '.$b.'<br>';
+        $c = $b*10;
+        echo 'Contenido de $c: '.$c.'<br>';
+        $a .= $b;
+        echo 'Contenido de $b: '.$b.'<br>';
+        $b *= $c;
+        echo 'Contenido de $c: '.$c.'<br>';
+        $z[0] = "MySQL"; 
+        echo 'Contenido de $z[]: '; print_r($z);
+    ?>
+
+    <h1>Inciso 4</h1>
+    <p>Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de
+        la matriz $GLOBALS o del modificador global de PHP. </p>
+    
+    <?php
+        echo 'Contenido de $a en el ambito global: '.$GLOBALS['a']."<br>";
+        echo 'Contenido de $b en el ambito global: '.$GLOBALS['b']."<br>";
+        echo 'Contenido de $c en el ambito global: '.$GLOBALS['c']."<br>";
+        echo 'Contenido de $z en el ambito global: no es posible leerlo con la super variable $GLOBALS ya que no esta disponible 
+        a partir de la version PHP 8.1.0 '."<br>"; //Pendiente por revisar con global
+    ?>
+
+    <h1>Inciso 5</h1>
+    <p>5. Dar el valor de las variables $a, $b, $c al final del siguiente script: </p>
+    <p> $a = “7 personas”;<br>
+        $b = (integer) $a;<br>
+        $a = “9E3”;<br>
+        $c = (double) $a;<br> </p>
+
+    <?php
+    $a = "7 personas";
+    $b = (integer) $a;
+    $a = "9E3";
+    $c = (double) $a;
+
+    echo 'Contenido de $a: '.$a.'<br>';
+    echo 'Contenido de $b: '.$b.'<br>';
+    echo 'Contenido de $c: '.$c.'<br>';
+    ?>
+
+    <h1>Inciso 6</h1>
+    <p>6. Dar y comprobar el valor booleano de las variables $a, $b, $c, $d, $e y $f y muéstralas
+        usando la función var_dump(<datos>).</p>
+    <p>Después investiga una función de PHP que permita transformar el valor booleano de $c y $e
+    en uno que se pueda mostrar con un echo:<br>
+        $a = “0”;<br>
+        $b = “TRUE”;<br>
+        $c = FALSE;<br>
+        $d = ($a OR $b);<br>
+        $e = ($a AND $c);<br>
+        $f = ($a XOR $b);<br></p>
+    
+
 </body>
 </html>
