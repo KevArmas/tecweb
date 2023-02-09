e40776ffbd7f023ce4eda906e79cb1595724d6db
KevArmas <kevin.armas@alumno.buap.mx>
2023-02-08 21:50:57 -0600

    Respuesta punto 2

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -44,7 +44,32 @@
         <p>
             R:
             <?php
-            // AQUÍ VA EL CÓDIGO DE SOLUCIÓN
+            $iteraciones = 0;
+            $Numeros = 0;
+            $matriz = array();
+            
+            while (true) {
+              $iteraciones++;
+              for ($i = 0; $i < 3; $i++) {
+                $num = rand(1, 100);
+                $matriz[$iteraciones - 1][$i] = $num;
+                $Numeros++;
+              }
+              if ($matriz[$iteraciones - 1][0] % 2 != 0 && $matriz[$iteraciones - 1][1] % 2 == 0 && $matriz[$iteraciones - 1][2] % 2 != 0) {
+                break;
+              }
+            }
+            
+            echo "Numero de iteraciones: " . $iteraciones . "<br>";
+            echo "Total de numeros generados: " . $Numeros . "<br>";
+            echo "Matriz: <br>";
+            
+            for ($i = 0; $i < $iteraciones; $i++) {
+              for ($j = 0; $j < 3; $j++) {
+                echo $matriz[$i][$j] . " ";
+              }
+              echo "<br>";
+            }
             ?>
         </p>
     </div>
