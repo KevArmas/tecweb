9dc223d953dccdb02664116897ba1c1a96c6059f
KevArmas <kevin.armas@alumno.buap.mx>
2023-03-04 14:11:35 -0600

    Correcion en validacion php y prueba de funcionamiento con imagenes

--- practicas/p06/set_producto_v2.php
+++ practicas/p06/set_producto_v2.php
@@ -16,71 +16,67 @@
 		</style>
 	</head>
 	<body>
-		<h1>Producto añadido correctamente</h1>
+    <?php
+            $nombre = 'nombre_producto';
+            $marca  = 'marca_producto';
+            $modelo = 'modelo_producto';
+            $precio = 1.0;
+            $detalles = 'detalles_producto';
+            $unidades = 1;
+            $imagen   = 'img/imagen.png';
+            $eliminado = 0;
 
-		<h2>Producto:</h2>
-		<ul>
-			<li><strong>Nombre:</strong> <em><?php echo $_POST['nombre']; ?></em></li>
-			<li><strong>Marca:</strong> <em><?php echo $_POST['marca']; ?></em></li>
-			<li><strong>Modelo:</strong> <em><?php echo $_POST['modelo']; ?></em></li>
-            <li><strong>Precio:</strong> <em><?php echo $_POST['precio']; ?></em></li>
-            <li><strong>Unidades:</strong> <em><?php echo $_POST['unidades']; ?></em></li>
-            <li><strong>Detalles:</strong> <em><?php echo $_POST['detalles']; ?></em></li>
-            <li><strong>Imagen:</strong> <em><?php echo $_POST['imagen']; ?></em></li>
-		</ul>
+            /** SE CREA EL OBJETO DE CONEXION */
+            $link = new mysqli('localhost', 'root', '1983270828040110', 'marketzone');	
+
+            /** comprobar la conexión */
+            if ($link->connect_errno) 
+            {
+                die('Falló la conexión: '.$link->connect_error.'<br/>');
+                /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+            }
+            //Revisa que el formulario haya sido enviado y cacha marca y modelo con el metodo $_POST
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $marca = $_POST["marca"];
+                $modelo = $_POST["modelo"];
+
+            //Selecciona las marcas y modelos enviados del formulario
+            $sql = "SELECT * from productos WHERE marca = '$marca' AND modelo = '$modelo' ";
+            $result = $link->query($sql);
+
+            //Revisa si el resultado ya se encuentra en la tabla
+            if($result !== false && $result->num_rows > 0) {
+                echo "La marca y modelo ya existen";
+            }
+            else { //Si no hay registros de la marca y el modelo entonces envia los datos a la base de datos
+                $nombre = $_POST["nombre"];
+                $marca  = $_POST["marca"];
+                $modelo = $_POST["modelo"];
+                $precio = $_POST["precio"];
+                $detalles = $_POST["detalles"];
+                $unidades = $_POST["unidades"];
+                $imagen   = $_POST["imagen"];
+
+            $sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) VALUES ('$nombre', '$marca', '$modelo', '$precio', '$detalles', '$unidades', '$imagen')"; 
+            
+            //Manda mensaje que la insercion fue correcta y el id del producto insertado
+            if ($link->query($sql) === TRUE) {
+                echo 'Producto insertado con ID: '.$link->insert_id;
+            } 
+            else {
+                echo "Error al añadir el producto" .$link->error;
+            }
+            }
+
+            $link->close();
+        }
+            ?>
+
 		<p>
 		    <a href="http://validator.w3.org/check?uri=referer"><img
 		      src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
 		</p>
 
-
-        <?php
-$nombre = 'nombre_producto';
-$marca  = 'marca_producto';
-$modelo = 'modelo_producto';
-$precio = 1.0;
-$detalles = 'detalles_producto';
-$unidades = 1;
-$imagen   = 'img/imagen.png';
-
-/** SE CREA EL OBJETO DE CONEXION */
-@$link = new mysqli('localhost', 'root', '1983270828040110', 'marketzone');	
-
-/** comprobar la conexión */
-if ($link->connect_errno) 
-{
-    die('Falló la conexión: '.$link->connect_error.'<br/>');
-    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
-}
-
-//Selecciona las marcas y modelos enviados del formulario
-$sql = "SELECT * from productos WHERE brand = '$marca' AND model = '$modelo' ";
-$result = $link->query($sql);
-
-//Revisa si el resultado ya se encuentra en la tabla
-if($result->num_rows > 0) {
-    echo "La marca y modelo ya existen";
-}
-else { //Si no hay registros de la marca y el modelo entonces envia los datos a la base de datos
-    $nombre = $_POST["nombre"];
-    $marca  = $_POST["marca"];
-    $modelo = $_POST["modelo"];
-    $precio = $_POST["precio"];
-    $detalles = $_POST["detalles"];
-    $unidades = $_POST["unidades"];
-    $imagen   = $_POST["imagen"];
-
- $sql = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) VALUES ('$nombre', '$marca', '$modelo', '$precio', '$unidades', '$detalles', '$imagen')"; 
- 
- if ($link->query($sql) === TRUE) {
-    echo 'Producto insertado con ID: '.$link->insert_id;
- } 
- else {
-    echo "Error al añadir el producto" .$link->error;
- }
-}
-
-$link->close();
-?>
+        
 	</body>
 </html>
